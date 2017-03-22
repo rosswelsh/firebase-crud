b02124cac310c41c339526a7b5abb998fc0e6cd2
ross welsh <rosswelsh@hotmail.com>
2017-03-22 15:48:35 +0000

    parts 1 and 2 of angular tutorial, added a home page and login component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import {AF} from './providers/af';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { HomePageComponent } from './home-page/home-page.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginPageComponent,
+    HomePageComponent
   ],
   imports: [
     BrowserModule,
@@ -15,6 +21,20 @@ import { AppComponent } from './app.component';
     HttpModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  providers: [AF]
 })
 export class AppModule { }
+
+<script src="https://www.gstatic.com/firebasejs/3.7.3/firebase.js"></script>
+  <script>
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyB_6WPLyMkW3XRDiJvbQWePRFSRiYKN38k",
+  authDomain: "learnjavascript-fb214.firebaseapp.com",
+  databaseURL: "https://learnjavascript-fb214.firebaseio.com",
+  storageBucket: "learnjavascript-fb214.appspot.com",
+  messagingSenderId: "584049379460"
+};
+firebase.initializeApp(config);
+</script>
